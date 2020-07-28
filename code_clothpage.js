1697315a1967b07f77f86a621302746627fe8bc1
BBondarenko1812 <57586039+BBondarenko1812@users.noreply.github.com>
2020-07-28 12:08:14 +0300

    product page 2.0

--- code_clothpage.js
+++ code_clothpage.js
@@ -1,26 +1,35 @@
 var section = document.getElementById("section"),
-item = null, part = null;
+item = null, part = null, piece = null;
 
-function goToPage(start_cloth,end_cloth)
+function goToPage(start_cloth,end_cloth) 
 {
-	window.location.href = "index_clothpage.html";
-	sessionStorage.setItem("page_choice_start",start_cloth);
-	sessionStorage.setItem("page_choice_end",end_cloth);
+    // going from the main page to the clothpage adding start and end coordinates to local storage 
+    localStorage.setItem('start_cloth',start_cloth);
+    localStorage.setItem('end_cloth',end_cloth);
+	window.location.assign("index_clothpage.html");
 }
 
-function onClick(start_cloth,end_cloth)
+function pageLoaded()
+{
+    creatingContentList(localStorage.getItem('start_cloth'),localStorage.getItem('end_cloth'));
+}
+
+function onClick_button(start_cloth,end_cloth)
 {
 	section.innerHTML = "";
-	creatingContent(start_cloth,end_cloth);
+    creatingContentList(start_cloth,end_cloth);
 }
 
-function creatingContent(start_cloth,end_cloth)
+function creatingContentList(start_cloth,end_cloth)
 {	
 	for(var i = start_cloth; i < end_cloth; i ++)
 	{
+      
    	 	item = document.createElement("div");
    	 	item.classList.add("col-sm");
    	 	item.classList.add("item-block");
+        let item_block = document.querySelector('.item-block');
+        $(item).attr("onClick", `creatingContentPage(`+i+`)`);
 
     	part = document.createElement("img");
     	part.classList.add("p-img");
@@ -37,9 +46,50 @@ function creatingContent(start_cloth,end_cloth)
     	part.innerHTML = t_shirt[i].price;
     	item.appendChild(part);
 
-    	item.appendChild(part);
     	section.appendChild(item);
     }
+}
+
+function creatingContentPage(position_item)
+{
+    console.log(position_item);
+    section.innerHTML = "";
+
+    item = document.createElement("div");
+    item.classList.add("row");
+    item.classList.add("item-block-own-page");
+
+    part = document.createElement("div");
+    part.classList.add("col-xxl");
+    part.classList.add('clothpage-h-block')
+
+    piece = document.createElement("h2");
+    piece.innerHTML = "T-shirt Black";
+    piece.classList.add("clothpage-h");
+
+    part.appendChild(piece);
+    item.appendChild(part);
+
+    part = document.createElement("div");
+    part.classList.add("col-md");
+
+    piece = document.createElement("img");
+    piece.classList.add("clothpage-img");
+    piece.src = t_shirt[position_item].img;
+
+    part.appendChild(piece);
+    item.appendChild(part);
+
+    part = document.createElement("div");
+    part.classList.add("col-md")
+
+    piece = document.createElement("h3");
+    piece.innerHTML = t_shirt[position_item].price;
+    piece.classList.add("clothpage-price");
+
+    part.appendChild(piece);
+    item.appendChild(part);
+    section.appendChild(item);
 }
 
 
